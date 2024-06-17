6c338430bac9901c08f183291241defe43e88a57
Hicham Benjelloun <hbenjelloun@pm.me>
2024-06-17 16:59:25 +0200

    update prettier config file

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import styled, { css } from "styled-components";
+import React from 'react';
+import styled, { css } from 'styled-components';
 
-import { COLORS } from "../../constants";
+import { COLORS } from '../../constants';
 
 const sizeStyles = {
   small: css`
